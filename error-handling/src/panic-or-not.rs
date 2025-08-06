d2b424dafa44a4c83200e41d4219697b349fc314
Glimmerina <Jchallenorstaffs@gmail.com>
2025-08-06 20:47:18 +0100

    Added chapter 9.3

---
+++ error-handling/src/panic-or-not.rs
@@ -0,0 +1,11 @@
+fn main() {
+    use std::net::IpAddr;
+
+    let home: IpAddr = "127.0.0.1"
+        .parse()
+        .expect("Hardcoded IP address should be valid");
+
+    // The rest of the code from this section wanted me to use the Guessing game from chapter 2.
+    // However I have already modified that code into my own version so it's a little late.
+    // Still, the point makes sense. Refer back to this chapter when error handling.
+}

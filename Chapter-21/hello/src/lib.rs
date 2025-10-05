242baaafb4f1899b47b9eed2a583b25f74942acf
Glimmerina <Jchallenorstaffs@gmail.com>
2025-10-05 19:49:47 +0100

    Added chapter 21.2

---
+++ Chapter-21/hello/src/lib.rs
@@ -0,0 +1,79 @@
+// Uses the standard library for concurrency.
+use std::{
+    sync::{Arc, Mutex, mpsc},
+    thread,
+};
+
+// Creates a struct for the threadpool.
+// It consists of workers that will execute the jobs sent to the pool.
+// It also includes a sender to send jobs to the workers.
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+// Creates a struct for Jobs.
+struct Job;
+
+// Creates a struct for workers that includes ID and a thread.
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+// Implements the worker struct. 
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        // Spawns a new thread for each worker.
+        let thread = thread::spawn(move || {
+            // Each worker will wait for a job to be sent to it.
+            while let Ok(job) = receiver.lock().unwrap().recv() {
+                println!("Worker {id} got a job; executing.");
+
+                job();
+            }
+        });
+
+        // Returns the worker with its ID and thread.
+        Worker { id, thread }
+    }
+}
+
+// Implementation of the ThreadPool struct.
+// The tutorial had me remake this code block so many times. 
+// So many comments had to be purged. So many. Comment exterminatus.
+impl ThreadPool {
+
+    // Creates a new thread pool with a given size. Asserts that the size is greater than 0.
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        // Creates a channel for sending and receiving jobs.
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+        // Creates a vector to hold the workers. It's mutable and has a capacity of the given size.
+
+        let mut workers = Vec::with_capacity(size);
+
+        // For each job id, creates a new worker and adds it to the workers vector.
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool { workers, sender }
+    }
+
+    // Executes a job by sending it.
+    
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+
+        self.sender.send(job).unwrap();
+    }
+}
+
+
--- Chapter-21/hello/src/main.rs
+++ Chapter-21/hello/src/main.rs
@@ -1,21 +1,33 @@
 
 // Uses the io crate for input/output operations and the net crate for tcp networking. 
 // *update now also uses the filesystem crate.  
+// **Update now also uses the thread and time crates.
 use std::{
     fs,
     io::{BufReader, prelude::*},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
+
+// Calls the threadpool from the lib.rs file.
+use hello::ThreadPool;
 
 fn main() {
     // Binds the listener to the address and port.
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-
+    // Creates 4 threads in a thread pool. 
+    // This limits the number of threads that can be spawned at once to 4.
+    // If more than 4 connections are made, the additional connections will wait until a thread is available.
+    // I wonder if this helps with DDOS attacks?
+    let pool = ThreadPool::new(4);
     // For each incoming connection, handle the connection by calling the handle_connection function.
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -35,10 +47,13 @@ fn handle_connection(mut stream: TcpStream) {
 
     // Print the request line and the full HTTP request to the console.
     // If it's valid, load the hello file. If not, error 404.
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
 
     // Read the contents of the file and get its length.
